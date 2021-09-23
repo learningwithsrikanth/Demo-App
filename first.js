5604a5b3d9555f2bba420a2939dccb8517fb270a
learningwithsrikanth <90610817+learningwithsrikanth@users.noreply.github.com>
2021-09-23 20:53:27 +0530

    removed subtract feature

--- first.js
+++ first.js
@@ -4,10 +4,3 @@ function add(num1, num2) {
 }
 let result = add(1, 2)
 console.log(result)
-
-function subtract(num1, num2) {
-    let result = num1 - num2
-    return result
-}
-let result = subtract(2, 1)
-console.log(result)
