64a41635020918966aab0f0804513827a1b06bde
learningwithsrikanth <90610817+learningwithsrikanth@users.noreply.github.com>
2021-09-23 20:46:40 +0530

    Update first.js

--- first.js
+++ first.js
@@ -1,7 +1,13 @@
-function add(num1,num2){
-    let sum =num1+num2
-    console.log(num1 +num2)
-
+function add(num1, num2) {
+    let sum = num1 + num2
+    return sum
 }
-let result=add(1,2)
+let result = add(1, 2)
+console.log(result)
+
+function subtract(num1, num2) {
+    let result = num1 - num2
+    return result
+}
+let result = subtract(2, 1)
 console.log(result)
